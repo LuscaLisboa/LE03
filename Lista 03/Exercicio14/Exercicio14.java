f8a9f5266c3519f150bd3359a438666db66dc277
Lucas Gabriel Lisboa <lglisboa1503@gmail.com>
2024-04-12 17:52:02 -0300

    .gitignore

---
+++ Lista 03/Exercicio14/Exercicio14.java
@@ -0,0 +1,15 @@
+package Exercicio14;
+
+import java.util.Scanner;
+
+public class Exercicio14 {
+    public static void execute(){
+
+        Scanner read = new Scanner(System.in);
+
+        
+        
+        read.close();
+    }
+}
+
