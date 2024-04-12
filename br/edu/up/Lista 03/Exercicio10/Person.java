34f3b052aa0a238b69686f2db3336a5f13191c11
Lucas Gabriel Lisboa <lglisboa1503@gmail.com>
2024-04-12 18:20:02 -0300

    Deletando commit errado

---
+++ br/edu/up/Lista 03/Exercicio10/Person.java
@@ -0,0 +1,14 @@
+package Exercicio10;
+
+public class Person {
+
+    private int age;
+
+    // Age
+    public void setAge(int sAge){
+        this.age = sAge;
+    }
+    public int getAge(){
+        return this.age;
+    }
+}
