7ce6165201c35fc59696df9b7e7530cb4b9ba101
Lucas G. Lisboa <lglisboa1503@gmail.com>
2024-04-26 20:12:23 -0300

    teste pull

--- br/edu/up/Lista 03/Exercicio19/Exercicio19.java
+++ br/edu/up/Lista 03/Exercicio19/Exercicio19.java
@@ -1,24 +1,23 @@
 package Exercicio19;
+
 import java.util.Scanner;
 
 public class Exercicio19 {
-    public void executar(){
-        Scanner leitor = new Scanner(System.in);
-        Triangulo triangulo = new Triangulo();
-        System.out.println("Informe três valores referentes aos lados de um triângulo: ");
-        int lado1 = leitor.nextInt();
-        int lado2 = leitor.nextInt();
-        int lado3 = leitor.nextInt();
+    public void executar() {
 
-        if (triangulo.formaTriangulo(lado1, lado2, lado3)) {
-            if (lado1 == lado2 && lado2 == lado3) {
-                System.out.println("Triângulo equilatero");
-            }else if (lado1 == lado2 || lado1 == lado2 || lado2 == lado3) {
-                System.out.println("Triangulo isosceles");
-            }else{
-                System.out.println("Triangulo escaleno");
-            }
-        }
+        Scanner leitor = new Scanner(System.in);
+
+        Triangulo triangulo = new Triangulo();
+
+        System.out.println("Informe o 1° lado do triângulo: ");
+        triangulo.setLado1(leitor.nextInt());
+        System.out.println("Informe o 2° lado do triângulo: ");
+        triangulo.setLado2(leitor.nextInt());
+        System.out.println("Informe o 3° lado do triângulo: ");
+        triangulo.setLado3(leitor.nextInt());
+
+        
+
         leitor.close();
     }
 }
--- br/edu/up/Lista 03/Exercicio19/Triangulo.java
+++ br/edu/up/Lista 03/Exercicio19/Triangulo.java
@@ -1,8 +1,30 @@
 package Exercicio19;
 
 public class Triangulo {
-    public static boolean formaTriangulo(int lado1,int lado2, int lado3){
-        return (lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1);
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    public double getLado1() {
+        return lado1;
     }
-    //Verificar se três valores inteiros podem formar os lados de um triângulo!
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+    
+    public double getLado3() {
+        return lado3;
+    }
+    public void setLado3(double lado3) {
+        this.lado3 = lado3;
+    }
+
+    
 }
