591b77da2008f661ffaef0d48e64d9aab165a696
Lucas G. Lisboa <lglisboa1503@gmail.com>
2024-04-26 20:46:33 -0300

    Correção Exercício 21

--- br/edu/up/Lista 03/Exercicio21/Nadador.java
+++ br/edu/up/Lista 03/Exercicio21/Nadador.java
@@ -1,13 +1,20 @@
 package Exercicio21;
 
-public class Nadador{
+public class Nadador {
     private int idade;
-    public Nadador(int idade){
+
+    public Nadador(int idade) {
         this.idade = idade;
     }
-    public int getIdade(){
+
+    public int getIdade() {
         return idade;
     }
+
+    public void setIdade(int sIdade) {
+        this.idade = sIdade;
+    }
+
     public Nadador() {
     }
 }
